6e00e486fbf5789dd9826b791460d319767fdcab
Alcybr <aslahaddin@gmail.com>
2023-07-09 22:48:42 -0500

    text_attacks: importance-ranked deletion + TextFooler-style synonym swap

--- src/text_attacks.py
+++ src/text_attacks.py
@@ -49,3 +49,114 @@ def char_noise(text: str, n_perturbations: int = 3, seed: int = 0) -> str:
     return ''.join(chars)
 
 
+def word_importance(
+    predict_proba: Callable[[str], List[float]],
+    text: str,
+    target_class: int,
+) -> List[float]:
+    """For each word, score importance as drop in target-class prob when removed."""
+    words = text.split()
+    base = predict_proba(text)[target_class]
+    scores = []
+    for i in range(len(words)):
+        masked = ' '.join(words[:i] + words[i+1:])
+        new = predict_proba(masked)[target_class]
+        scores.append(base - new)
+    return scores
+
+
+def importance_ranked_deletion(
+    predict_proba: Callable[[str], List[float]],
+    text: str,
+    original_pred: int,
+    max_deletions: int = 5,
+) -> AttackResult:
+    """Greedy attack: delete words in importance order until prediction flips."""
+    words = text.split()
+    queries = 0
+    scores = word_importance(predict_proba, text, original_pred)
+    queries += len(words) + 1
+    order = sorted(range(len(words)), key=lambda i: -scores[i])
+
+    current_text = text
+    for k in range(1, max_deletions + 1):
+        keep = sorted(order[k:])
+        candidate = ' '.join(words[i] for i in keep)
+        probs = predict_proba(candidate)
+        queries += 1
+        new_pred = max(range(len(probs)), key=lambda j: probs[j])
+        if new_pred != original_pred:
+            return AttackResult(
+                original_text=text,
+                adversarial_text=candidate,
+                queries=queries,
+                succeeded=True,
+                original_pred=original_pred,
+                final_pred=new_pred,
+            )
+    return AttackResult(
+        original_text=text,
+        adversarial_text=current_text,
+        queries=queries,
+        succeeded=False,
+        original_pred=original_pred,
+        final_pred=original_pred,
+    )
+
+
+def synonym_swap_attack(
+    predict_proba: Callable[[str], List[float]],
+    text: str,
+    original_pred: int,
+    synonym_dict: dict,
+    max_swaps: int = 5,
+) -> AttackResult:
+    """TextFooler-style synonym replacement.
+
+    `synonym_dict` maps each candidate word to a list of synonyms. In
+    practice you'd use counter-fitted GloVe + a sense filter; here we
+    parameterize so callers can plug in whatever vocabulary they want.
+    """
+    words = text.split()
+    queries = 0
+    base_scores = word_importance(predict_proba, text, original_pred)
+    queries += len(words) + 1
+    ranked = sorted(range(len(words)), key=lambda i: -base_scores[i])
+
+    current = list(words)
+    for idx in ranked[:max_swaps]:
+        word = current[idx].lower().strip(string.punctuation)
+        candidates = synonym_dict.get(word, [])
+        best_drop = 0.0
+        best_word = None
+        for cand in candidates:
+            trial = list(current)
+            trial[idx] = cand
+            probs = predict_proba(' '.join(trial))
+            queries += 1
+            drop = base_scores[idx] - (probs[original_pred] - 0.0)
+            if drop > best_drop:
+                best_drop = drop
+                best_word = cand
+        if best_word:
+            current[idx] = best_word
+            probs = predict_proba(' '.join(current))
+            queries += 1
+            new_pred = max(range(len(probs)), key=lambda j: probs[j])
+            if new_pred != original_pred:
+                return AttackResult(
+                    original_text=text,
+                    adversarial_text=' '.join(current),
+                    queries=queries,
+                    succeeded=True,
+                    original_pred=original_pred,
+                    final_pred=new_pred,
+                )
+    return AttackResult(
+        original_text=text,
+        adversarial_text=' '.join(current),
+        queries=queries,
+        succeeded=False,
+        original_pred=original_pred,
+        final_pred=original_pred,
+    )
